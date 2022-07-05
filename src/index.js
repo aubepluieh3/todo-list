05ec06b72bd612a3f918e2eac93eb9e80b98b8b5
SOO <aubepluieh3@naver.com>
2022-07-05 17:44:23 +0900

    redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './styles/main.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import modules from './modules';
+import {legacy_createStore as createStore} from "redux"
+import {Provider} from 'react-redux';
+
+const store = createStore(modules, window.devToolsExtension && window.devToolsExtension());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
